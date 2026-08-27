569d9f87304e32a3578a769247d222fae38dd562
Alvaro Nina <alvaro.nina.v@ubc.edu.bo>
2026-08-27 16:00:02 +0200

    feat: validar multiplos de ambos casos

--- src/catalogador.test.js
+++ src/catalogador.test.js
@@ -4,12 +4,13 @@ describe("Catalogar", () => {
   it("deberia salir Fizz por ser multiplo de 3", () => {
     expect(catalogar(12)).toEqual("Fizz");
   });
-  /*it("deberia salir Buzz por ser multiplo de 5", () => {
+  it("deberia salir Buzz por ser multiplo de 5", () => {
     expect(catalogar(10)).toEqual("Buzz");
-  });*/
-  /*it("deberia salir FizzBuzz por ser multiplo de 3 y 5", () => {
+  });
+  it("deberia salir FizzBuzz por ser multiplo de 3 y 5", () => {
     expect(catalogar(15)).toEqual("FizzBuzz");
   });
+  /*
   it("deberia salir el mismo numero si no es multiplo de 3 ni 5", () => {
     expect(catalogar(13)).toEqual(13);
   });
