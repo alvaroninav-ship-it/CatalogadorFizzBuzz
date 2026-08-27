8b1402303fe3767848bb5d5baca1ed2929c48344
Alvaro Nina <alvaro.nina.v@ubc.edu.bo>
2026-08-27 16:01:08 +0200

    feat: validar iguales

--- src/catalogador.test.js
+++ src/catalogador.test.js
@@ -10,10 +10,11 @@ describe("Catalogar", () => {
   it("deberia salir FizzBuzz por ser multiplo de 3 y 5", () => {
     expect(catalogar(15)).toEqual("FizzBuzz");
   });
-  /*
+  
   it("deberia salir el mismo numero si no es multiplo de 3 ni 5", () => {
     expect(catalogar(13)).toEqual(13);
   });
+  /*
   it("deberia salir 0 si el numero es 0", () => {
     expect(catalogar(0)).toEqual(0);
   });*/
